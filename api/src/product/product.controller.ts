fc9b99970d302778380c958aeb5ed92f4f72c2ef
Jay Kapade <jazzdestructor@gmail.com>
2022-09-25 18:15:39 +0530

    Added controller paths and functions for products

--- api/src/product/product.controller.ts
+++ api/src/product/product.controller.ts
@@ -1,13 +1,41 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Delete,
+} from '@nestjs/common';
 import { ProductDto } from 'src/dto/product.dto';
+import { ProductDocument } from './product.schema';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
 
+  @Get()
+  findAllProducts(): Promise<ProductDocument[]> {
+    return this.productService.findAllProducts();
+  }
+  @Get(':id')
+  findProduct(@Param('id') id: string): Promise<ProductDocument> {
+    return this.productService.findProduct(id);
+  }
   @Post()
-  createProduct(@Body() productInfo: ProductDto) {
+  createProduct(@Body() productInfo: ProductDto): Promise<ProductDocument> {
     return this.productService.createProduct(productInfo);
+  }
+  @Patch(':id')
+  updateProduct(
+    @Param('id') id: string,
+    @Body() newProductInfo,
+  ): Promise<ProductDocument> {
+    return this.productService.updateProduct(id, newProductInfo);
+  }
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productService.deleteProduct(id);
   }
 }
--- api/src/product/product.service.ts
+++ api/src/product/product.service.ts
@@ -22,4 +22,81 @@ export class ProductService {
       );
     }
   }
+
+  async findAllProducts(): Promise<ProductDocument[]> {
+    //exec used to get Promise find does return thenable similar to promise, its just better practice
+    return await this.productModel.find().exec();
+  }
+
+  async findProduct(id: string): Promise<ProductDocument> {
+    //exec used to get Promise find does return thenable similar to promise, its just better practice
+    try {
+      const product = await this.productModel.findById(id).exec();
+      if (!product)
+        throw new HttpException(
+          { message: `No Product with id: ${id} found` },
+          HttpStatus.NOT_FOUND,
+        );
+      return product;
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async updateProduct(
+    id: string,
+    newProductInfo: ProductDto,
+  ): Promise<ProductDocument> {
+    try {
+      //exec used to get Promise find does return thenable similar to promise, its just better practice
+      //Check if empty object
+      if (Object.keys(newProductInfo).length === 0)
+        throw new HttpException(
+          'Needs data to update the product',
+          HttpStatus.BAD_REQUEST,
+        );
+      const product = await this.productModel.findByIdAndUpdate(
+        id,
+        newProductInfo,
+        {
+          new: true,
+        },
+      );
+
+      if (!product) {
+        throw new HttpException(
+          `Product with id: ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+
+      return product;
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  async deleteProduct(id: string) {
+    try {
+      const product = await this.productModel.findByIdAndDelete(id);
+      if (!product) {
+        throw new HttpException(
+          `Product with id: ${id} not found`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      return { success: true, message: 'Product was deleted successfully' };
+    } catch (error) {
+      throw new HttpException(
+        { success: false, message: error.message },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
 }
